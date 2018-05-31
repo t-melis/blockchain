ae4f021b0ff9d86570df27bbabbe27e49707a1cb
Thanos Melistas <thanos.melistas@gmail.com>
2018-06-01 00:42:59 +0300

    add proofOfWork

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,5 +1,4 @@
 const sha256 = require('sha256');
-
 function Blockchain(){
     this.chain = [];
     this.pendingTransactions = [];
@@ -38,9 +37,21 @@ Blockchain.prototype.createNewTransaction = function(amount, sender, recipient){
     return this.getLastBlock()['index'] + 1;
 }
 
-Blockchain.prototype.hashBlock(previousBlockHash, currentBlockData, nonce){
+Blockchain.prototype.hashBlock = function(previousBlockHash, currentBlockData, nonce){
     const dataAsString = previousBlockHash + nonce.toString() + JSON.stringify(currentBlockData);
     const hash = sha256(dataAsString);
+    return hash;
+}
+
+Blockchain.prototype.proofOfWork = function(previousBlockHash, currentBlockData){
+    let nonce = 0;
+    let hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
+    while (hash.substring(0, 4) !== '0000'){
+        nonce++;
+        hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
+    }
+
+    return nonce;
 }
 
 module.exports = Blockchain;
--- dev/test.js
+++ dev/test.js
@@ -1,14 +1,26 @@
 const Blockchain = require('./blockchain');
-
 const bitcoin = new Blockchain();
 
-bitcoin.createNewBlock(123324,'ASDIM34IRM02', '123IEWMF013FQEFO');
-bitcoin.createNewTransaction(100, 'ALEXKASODNQ', 'JENMQOWINR1');
-bitcoin.createNewBlock(92412312424,'ASDMDMVOIA02M', 'ASMKVMIVAMODKF');
-bitcoin.createNewTransaction(100, 'ALEXKASODNQ', 'JENMQOWINR1');
-bitcoin.createNewTransaction(100, 'ALEXKASODNQ', 'JENMQOWINR1');
-bitcoin.createNewTransaction(100, 'ALEXKASODNQ', 'JENMQOWINR1');
-bitcoin.createNewBlock(92412312424,'ASDMDMVOIA02M', 'ASMKVMIVAMODKF');
+const previousBlockHash = 'AISJIM2341234K132MR0ERIFJ';
+const currentBlockData = [
+    {
+        amount: 10,
+        sender: 'AISDMSIAMDASD',
+        recipient: 'AISDMOMG0IRGMER'
+    },
+    {
+        amount: 30,
+        sender: 'AISDMSASDFDASD',
+        recipient: 'OMKNMGIAAWNERG'
+    },
+    {
+        amount: 100,
+        sender: 'WRQEFEFTHFDASD',
+        recipient: 'OMKASVFDBRGHTRIWNERG'
+    }
+];
+
+console.log(bitcoin.hashBlock(previousBlockHash, currentBlockData, 286979));
 
 
-console.log(bitcoin.chain[2]);
+
