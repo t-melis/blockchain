8bef65921edb8b95f6eee3b361c29aa329f57e68
Thanos Melistas <thanos.melistas@gmail.com>
2018-06-02 22:37:28 +0300

    add mine route

--- dev/api.js
+++ dev/api.js
@@ -1,22 +1,37 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 const bodyParser = require('body-parser');
+const Blockchain = require('./blockchain');
+
+const bitcoin = new Blockchain();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 
-app.get('/bloclchain', function( req, res ){
-    
+app.get('/blockchain', function( req, res ){
+    res.send(bitcoin);
 });
 
 app.post('/transaction', function(req, res){
-    console.log(req.body);
-    res.send('The amount of the transaction is ${req.body.amount} bitcoin');
+    const blockIndex = bitcoin.createNewTransaction(req.body.amount, req.body.sender, req.body.recipient);
+    res.json({note: 'Transaction will be added in block ' + blockIndex});
 });
 
 app.get('/mine', function(req, res){
-
+    const lastBlock = bitcoin.getLastBlock();
+    // we need last block's hash
+    const previousBlockHash = lastBlock['hash'];
+    const currentBlockData = {
+        transactions: bitcoin.pendingTransactions,
+        index: lastBlock['index'] + 1
+    };
+    
+    // we also need a nonce
+    const nonce = bitcoin.proofOfWork(previousBlockHash, currentBlockData);
+    const blockHash = bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce);
+    
+    const newBlock = bitcoin.createNewBlock(nonce, previousBlockHash, blockHash);
 });
 
 
